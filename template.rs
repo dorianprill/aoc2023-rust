e97d1120c8402676e4d9a6a14a408510bad95fe2
dorianprill <17551632+dorianprill@users.noreply.github.com>
2023-12-07 15:28:36 +0100

    simplify template to read full file to string

--- template.rs
+++ template.rs
@@ -1,33 +1,20 @@
-use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs::read_to_string;
 
 fn main() -> std::io::Result<()> {
     let input_file = "sample.txt";
 
-    // Get the current executable path
-    let mut exe_path = env::current_exe().expect("Failed to get the executable path");
-    // remove executable name and go up two directories
-    for _ in 0..3 {
-        exe_path.pop();
-    }
-    // Append the relative path to target directory
-    exe_path.push(input_file);
-
-    println!("Input file: {:?}", exe_path);
+    println!("Input file: {:?}", input_file);
 
     // read file line by line
-    let file = File::open(exe_path).expect("file not found");
-    let reader = BufReader::new(file);
+    let file_str = read_to_string(input_file)?;
 
     // iterate over lines
-    for line in reader.lines() {
-        let line = line?;
+    for (i, line) in file_str.lines().enumerate() {
         println!("{}", line);
         // Do stuff with the individual lines
     }
 
-    println!("Print results here");
+    println!("Print results");
 
     Ok(())
 }
