423c61345cc1b38ddb87251c56d439993194d42e
dorianprill <17551632+dorianprill@users.noreply.github.com>
2023-12-18 01:18:49 +0100

    add day 12 cargo project and inputs

---
+++ day12/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs::read_to_string;
+
+fn main() -> std::io::Result<()> {
+    let input_file = "sample.txt";
+
+    println!("Input file: {:?}", input_file);
+
+    // read file line by line
+    let file_str = read_to_string(input_file)?;
+
+    // iterate over lines
+    for (i, line) in file_str.lines().enumerate() {
+        println!("{}: {}", i, line);
+        // Do stuff with the individual lines
+    }
+
+    println!("Print results");
+
+    Ok(())
+}
+
